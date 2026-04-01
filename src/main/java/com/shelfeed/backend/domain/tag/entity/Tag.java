9c817681b6b37dd484b33b874333d1320c649402
SeungJo <seungjo.eren@gmail.com>
2026-04-01 22:13:29 +0900

    createEntiry (#11)
    독립 엔티티하고 내서제 엔티티 까지 만들었어요

--- src/main/java/com/shelfeed/backend/domain/tag/entity/Tag.java
+++ src/main/java/com/shelfeed/backend/domain/tag/entity/Tag.java
@@ -1,4 +1,20 @@
 package com.shelfeed.backend.domain.tag.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long tagId;
+
+    @Column(length = 100)
+    private String tagName;
 }
