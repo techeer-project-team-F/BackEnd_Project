552d5ab095a18e1f5be1be8ae4bbe66c36bb6857
seungjo <seungjo.eren@gmail.com>
2026-04-04 15:51:19 +0900

    인증부분 시큐리티 JPA 연결을 위한 파일 작성

--- src/main/java/com/shelfeed/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/shelfeed/backend/domain/member/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
-    Optional<Member> findByMemberUserId(String MemberUserId);
+    Optional<Member> findByMemberUserId(Long MemberUserId);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 }
---
+++ src/main/java/com/shelfeed/backend/global/security/CustomUserDetails.java
@@ -0,0 +1,48 @@
+package com.shelfeed.backend.global.security;
+
+import com.shelfeed.backend.domain.member.entity.Member;
+import com.shelfeed.backend.domain.member.enums.MemberStatus;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member){
+        this.member = member;
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(member.getMemberUserId());
+    }
+
+    @Override
+    public String getPassword(){
+        return member.getPassword();
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {//UserDetails인터페이스에 있는 메서드
+        return List.of(new SimpleGrantedAuthority("ROLE" + member.getRole().name()));
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return member.getStatus() != MemberStatus.SUSPENDED;
+    }
+
+    @Override
+    public  boolean isEnabled(){
+        return member.getStatus() != MemberStatus.WITHDRAWN;
+    }
+
+}
