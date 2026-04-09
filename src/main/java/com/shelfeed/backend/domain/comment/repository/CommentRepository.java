1ae0674ec9b794f631fd02ab68dd9cb1cddabc68
seungjo <seungjo.eren@gmail.com>
2026-04-09 14:15:55 +0900

    댓글 파트 레포지토리 까지 작성

---
+++ src/main/java/com/shelfeed/backend/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,32 @@
+package com.shelfeed.backend.domain.comment.repository;
+
+import com.shelfeed.backend.domain.comment.entity.Comment;
+import com.shelfeed.backend.domain.review.entity.Review;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    //특벙 감상의 원 댓글만 골라서 조회(페이지네이션 방식으로)
+    @Query(""" 
+            SELECT c FROM Comment c
+            WHERE c.review = :review
+            AND c.parentComment IS NULL
+            AND (:cursor IS NULL OR c.commentId < :cursor)
+            ORDER BY c.commentId DESC """) // 훨씬 간단하게 JPQL 작성하는 방법
+    List<Comment> findParentComments(@Param("review") Review review, @Param("cursor") Long cursor,
+                                     Pageable pageable);
+
+    // 대댓글 조회
+    List<Comment> findByParentComment(Comment parentComment);
+
+    // 삭제 안된 감상 조회
+    Optional<Comment> findByCommentIdAndIsDeletedFalse(Long commentId);
+
+    // 감상 삭제 시 댓들 모두 소프트 델리트 용도로 사용
+    List<Comment> findByReview (Review review);
+}
